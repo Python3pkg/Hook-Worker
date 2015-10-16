fbcbde842f1826f3e5b07e84ed1991d9241a9ef9
Thibault Clerice <leponteineptique@gmail.com>
2015-10-16 17:19:17 +0200

    Structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hook_worker',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Capitains/Hook-Worker',
+    license='GNU GPL',
+    author='Thibault Clerice',
+    author_email='leponteineptique@gmail.com',
+    description='Lightweight API to handle call and distribute them over a redis server'
+)
