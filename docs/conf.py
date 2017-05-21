99c05313ca52cddeb5d9553e2c21be04ab84a242
Python3pkg <raliclo@gmail.com>
2017-05-21 08:17:26 -0700

    Convert to Python3

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Hook Worker'
-copyright = u'2015, Thibault Clérice'
-author = u'Thibault Clérice'
+copyright = '2015, Thibault Clérice'
+author = 'Thibault Clérice'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -230,7 +230,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'HookWorker.tex', 'Hook Worker Documentation',
-   u'Thibault Clérice', 'manual'),
+   'Thibault Clérice', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
